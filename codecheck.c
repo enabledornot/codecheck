96fde3e310207255cf66d56e47a2c93a533fad83
enabledornot <102371948+enabledornot@users.noreply.github.com>
2022-09-06 21:38:21 -0400

    worked on counting code

--- codecheck.c
+++ codecheck.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void checkFile(FILE *file) {
+    char currentChar = fgetc(file);
+    int lineCount = 0;
+    int charCount = 0;
+    while(currentChar != EOF) {
+        if(currentChar=='\n') {
+            printf("Line %d Count %d\n",lineCount,charCount);
+            lineCount+=1;
+            charCount = 0;
+        }
+        else {
+            charCount+=1;
+        }
+        currentChar = fgetc(file);
+    }
+    printf("\n");
+}
 int main(int argc, char *argv[]) {
     FILE *file;
     if(argc>1 && (file = fopen(argv[1],"r"))) {
         printf("File Found\nReading File\n");
+        checkFile(file);
     }
     else {
         printf("Argument error\nPlease provide the path to a file as an argument\n");
